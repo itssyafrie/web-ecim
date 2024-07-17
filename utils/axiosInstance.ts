6112eff46da60837a51060bdba86843ba122f06a
Gerry Sheva <gerry.sheva@proton.me>
2024-07-17 09:52:29 +0700

    feat: implemented custom paramsSerializer for axios

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,14 +1,26 @@
-import axios from "axios";
+import axios, { AxiosInstance, ParamsSerializerOptions } from "axios";
 import {config} from "@/constants/url";
 
 
-const axiosInstance = axios.create({
+const axiosInstance: AxiosInstance = axios.create({
     withCredentials: true,
     baseURL: config.BASE_URL,
     timeout: 5000,
     headers: {
         "Content-type": "application/json"
-    }
+    },
+    paramsSerializer: {
+        serialize: (params: Record<string, any>): string => {
+            const keys = Object.keys(params);
+            const parts = keys.reduce((acc: string[], key: string) => {
+                if (params[key] !== null && params[key] !== undefined) {
+                    acc.push(`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+                }
+                return acc;
+            }, []);
+            return parts.join('&');
+        }
+    } as ParamsSerializerOptions
 })
 
 axiosInstance.interceptors.response.use(
