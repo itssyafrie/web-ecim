74fe41f3a4101e155edfd5c2937f319bd6818910
itssyafrie <itssyafrie@gmail.com>
2024-07-18 13:07:49 +0700

    feat: added multi step signup but still won't work

--- app/signup/selectrole/attendee/page.tsx
+++ app/signup/selectrole/attendee/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import Button from "@/components/ui/Button";
 import Container from "@/components/ui/Container";
 import Logo from "@/components/ui/Logo";
 import Link from "next/link";
 
-const Signup = () => {
+const Customer = () => {
   return (
     <main className="min-h-screen">
       <Container>
@@ -12,20 +14,15 @@ const Signup = () => {
             <div className="flex flex-col gap-y-6 justify-center items-center">
               <Logo />
               <h1 className="text-neutral-300 text-center font-light text-xl md:text-3xl">
-                Welcome to Ecim!
+                Just one more thing...
               </h1>
               <input
                 type="text"
-                placeholder="Enter your full name"
+                placeholder="Enter referral (optional)"
                 className="w-full px-4 h-[36px] text-neutral-300 bg-neutral-800 border rounded-lg border-neutral-700"
               />
-              <input
-                type="text"
-                placeholder="Enter your email address"
-                className="w-full px-4 h-[36px] text-neutral-300 bg-neutral-800 border rounded-lg border-neutral-700"
-              />
-              <Link href={"/signup/selectrole"}>
-                <Button label="Continue" />
+              <Link href={"/signup/selectrole/attendee"}>
+                <Button btnlarge label="Finish registration" />
               </Link>
             </div>
           </div>
@@ -35,4 +32,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Customer;
